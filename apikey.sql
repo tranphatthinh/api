7429c0bdb1d3387a390e497fba54db403f7177ca
manloplopo <1150080065@sv.hcmunre.edu.vn>
2025-03-24 21:51:49 +0700

    Initial commit - push all files

---
+++ apikey.sql
@@ -0,0 +1,6 @@
+CREATE TABLE apikeys (
+    id SERIAL PRIMARY KEY,
+    user_email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
